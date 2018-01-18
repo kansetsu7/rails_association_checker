0a27b37b76da0b2d9d4286bc4f9878ce2d80c428
Kansetsu Kuo <kansetsu4@gmail.com>
2018-01-18 10:29:07 +0800

    output rails convention on code panel when not given DB schema

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,16 @@
+// belongs_to :following, class_name: "User", foreign_key: "user_id", primary_key: "id"
+
 function check (){
   var inputs = [];
-  var modelName = document.getElementById("model-name").value
-  var relatoinInput = document.getElementById("relatoin-input").value
-  var myTableName = document.getElementById("my-table-name").value
-  var fk = document.getElementById("fk").value
-  var refTableName = document.getElementById("ref-table-name").value
-  var pk = document.getElementById("pk").value
+  inputs.push(document.getElementById("model-name").value);  // [0] modelName
+  inputs.push(document.getElementById("relatoin-input").value);  // [1] relatoinInput
+  inputs.push(document.getElementById("fk").value);  // [2] fk
+  inputs.push(document.getElementById("ref-table-name").value);  // [3] refTableName
+  inputs.push(document.getElementById("pk").value);  // [4] pk
 
   var codingPan = document.getElementById("coding-pan");
-  if (chkModelName(modelName, codingPan)) {
-    var map = chkRelationInput(relatoinInput, codingPan);
+  if (chkModelName(inputs[0], codingPan)) {
+    var map = chkRelationInput(inputs[1], codingPan);
     if (!map.get("chk")) {
       // alert("有錯喔");
       console.log("有錯喔");
@@ -17,17 +18,19 @@ function check (){
 
     
 
-    if (fk === "" && pk === "" && refTableName === "") {
-      console.log("all empty");
+    if (inputs[2] === "" && inputs[3] === "" && inputs[4] === "") {
+      // console.log("all empty");
       var relation = map.get("relation");
-      resultInPan(codingPan, modelName, relation);
+      resultInPan(codingPan, inputs[0], relation);
+      showRailsDefault(codingPan, inputs, relation);
+      console.log("yoooo");
       
     }
        
 
   }
 
-  console.log("yooo");
+  // console.log("yooo");
 }
 
 /*
@@ -38,7 +41,7 @@ function check (){
 function chkModelName(modelName, codingPan) {
   cleanPan(codingPan);
 
-  console.log("Checking model name...");
+  // console.log("Checking model name...");
   var errMsg = document.createElement("p");
   errMsg.style.color = "red";
 
@@ -59,7 +62,7 @@ function cleanPan(codingPan) {
   while (codingPan.lastChild != null) {
     codingPan.removeChild(codingPan.lastChild);
   }
-  console.log("Cleared!");
+  // console.log("Cleared!");
 }
 
 /*
@@ -68,7 +71,7 @@ function cleanPan(codingPan) {
 * return true if passed 
 */
 function chkCapitalize(string, codingPan) {
-  console.log("Checking first letter...");
+  // console.log("Checking first letter...");
 
   var errMsg = document.createElement("p");
   errMsg.style.color = "red";
@@ -77,7 +80,7 @@ function chkCapitalize(string, codingPan) {
     errMsg.innerHTML = "錯誤：Model名開頭不能為數字！";
     codingPan.appendChild(errMsg);
     return false;
-  }else if (capFirstLetter(string) === string) {
+  }else if (upFirstLetter(string) === string) {
     return true;
   }else{
     errMsg.innerHTML = "錯誤：Model名開頭需大寫！";
@@ -92,8 +95,7 @@ function chkCapitalize(string, codingPan) {
 * return map {("chk", false)} if pass
 */
 function chkRelationInput(relatoinInput, codingPan) {
-  // cleanPan(codingPan); //這行以後要拿掉
-  console.log("Checking relatoin...");
+  // console.log("Checking relatoin...");
   // set up msg 
   var errMsg = document.createElement("p");
   errMsg.style.color = "red";
@@ -114,11 +116,18 @@ function chkRelationInput(relatoinInput, codingPan) {
   for (var i = 0; i <= relation.length-1; i++) {
     relation2.push(relation[i].split(":"));
   }
-  relation_log(relation2);
   if (!chkRelationSymbol(relation2, codingPan)) {
     map.set("chk", false);
+    relation_log(relation2);
     return map;
   }
+
+  // trim space of relation arguments
+  for (var i = 0; i < relation2.length; i++) {
+    relation2[i][0] = relation2[i][0].trim();
+    relation2[i][1] = relation2[i][1].trim();
+  }
+  relation_log(relation2);
   map.set("chk", true);
   map.set("relation", relation2);
   return map;
@@ -130,11 +139,11 @@ function chkRelationInput(relatoinInput, codingPan) {
 * return false if not pass
 */
 function chkRelationSymbol(relation2, codingPan) {
-  console.log("Checking symbol...");
+  // console.log("Checking symbol...");
   var errMsg = document.createElement("p");
   errMsg.style.color = "red";
 
-  console.log(relation2[0].length);
+  // console.log(relation2[0].length);
   for (var i = 0; i <= relation2.length - 1; i++) {
     if (relation2[i].length != 2) {
       errMsg.innerHTML = "錯誤："+"關聯設定符號有問題，可能少了或多了冒號！";
@@ -150,12 +159,109 @@ function chkRelationSymbol(relation2, codingPan) {
   }
 }
 
+/*
+* 
+* 
+* 
+*/
+function showRailsDefault(codingPan, inputs, relation) {
+  console.log("showRailsDefault");
+  var resultElements = [];
+  resultElements.push(document.createElement("p"));
+  resultElements[0].classList.add("code-white");
+  resultElements[0].innerHTML = "====Rails convention====<br>";
+  resultElements.push(document.createElement("span"));
+  resultElements[1].classList.add("code-red");
+  resultElements[1].innerHTML = "Class ";
+  resultElements.push(document.createElement("span"));
+  resultElements[2].classList.add("code-green");
+  resultElements[2].innerHTML = inputs[0];
+  resultElements.push(document.createElement("span"));
+  resultElements[3].classList.add("code-white");
+  resultElements[3].innerHTML = " < ";
+  resultElements.push(document.createElement("span"));
+  resultElements[4].classList.add("code-green");
+  resultElements[4].innerHTML = "ApplicationRecord<br>";
+  resultElements.push(document.createElement("span"));
+  resultElements[5].classList.add("code-white");
+  resultElements[5].innerHTML = "&nbsp;&nbsp;belongs_to ";  //belongs_to
+  resultElements.push(document.createElement("span"));
+  resultElements[6].classList.add("code-purple");
+  resultElements[6].innerHTML = ":" + relation[0][1];
+  resultElements.push(document.createElement("span"));
+  resultElements[7].innerHTML = ", ";
+  resultElements[7].classList.add("code-white");
+  resultElements.push(document.createElement("span"));  //class_name
+  resultElements[8].innerHTML = "class_name: ";
+  resultElements[8].classList.add("code-purple");
+  resultElements.push(document.createElement("span"));
+  resultElements[9].innerHTML = "\"" + upFirstLetter(relation[0][1]) + "\"";
+  resultElements[9].classList.add("code-yellow");
+  resultElements.push(document.createElement("span"));
+  resultElements[10].innerHTML = ", ";
+  resultElements[10].classList.add("code-white");
+  resultElements.push(document.createElement("span"));  //foreign_key
+  resultElements[11].innerHTML = "foreign_key: ";
+  resultElements[11].classList.add("code-purple");
+  resultElements.push(document.createElement("span"));
+  resultElements[12].innerHTML = "\"" + relation[0][1] + "_id\"";
+  resultElements[12].classList.add("code-yellow");
+  resultElements.push(document.createElement("span"));
+  resultElements[13].innerHTML = ", ";
+  resultElements[13].classList.add("code-white");
+  resultElements.push(document.createElement("span"));  //primary_key
+  resultElements[14].innerHTML = "foreign_key: ";
+  resultElements[14].classList.add("code-purple");
+  resultElements.push(document.createElement("span"));
+  resultElements[15].innerHTML = "\"id\"";
+  resultElements[15].classList.add("code-yellow");
+  for (var i = 0; i < resultElements.length; i++) {
+    codingPan.appendChild(resultElements[i]);
+  }
+
+
+
+
+  // for (var i = 1; i < relation.length; i++) {
+  //   switch (relation[i][0]) {
+  //     case "class_name":
+  //       console.log("class_name");
+
+  //       break;
+  //     case "foreign_key":
+  //       console.log("foreign_key");
+  //       break;
+  //     case "primary_key":
+  //       console.log("primary_key");
+  //       break;
+  //     default:
+  //       errMsg.innerHTML = "參數" + (i+1) + "不符格式，應為class_name、foreign_key或primary_key其中之一。"
+  //       codingPan.appendChild(errMsg);
+  //       break;
+  //   }
+  // }
+}
+
+/*
+* check if the string have double quotes on both side
+*/
+function chkDoubleQuotes(str) {
+  return str === ("\"" + trimDQ(str) + "\"") ? true : false;
+}
+
 
 /*
 * capitalize first letter of the given string
 */
-function capFirstLetter(string) {
+function upFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/*
+* turn first letter of the given string to lowercase
+*/
+function lowFirstLetter(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
 }
 
 function relation_log(relation_array){
@@ -168,9 +274,14 @@ function relation_log(relation_array){
 }
 
 /*
-* 
-* 
-* 
+* trim the double quotes in string  
+*/
+function trimDQ(str) {
+  return str.replace(/\"/gm, "");
+}
+
+/*
+* write user input relation setup in code panel
 */
 function resultInPan(codingPan, modelName, relation) {
   var index = 6;
@@ -196,7 +307,7 @@ function resultInPan(codingPan, modelName, relation) {
   resultElements[5].innerHTML = "&nbsp;&nbsp;belongs_to ";
   resultElements.push(document.createElement("span"));
   resultElements[6].classList.add("code-purple");
-  resultElements[6].innerHTML = ":" + relation[0][1].trim();
+  resultElements[6].innerHTML = ":" + relation[0][1];
 
   for (var i = 1; i < relation.length; i++) {
     index2 = index + 3 * ( i - 1 );
@@ -205,15 +316,15 @@ function resultInPan(codingPan, modelName, relation) {
     resultElements[index2+1].classList.add("code-white");
 
     resultElements.push(document.createElement("span"));
-    resultElements[index2+2].innerHTML = relation[i][0].trim() + ": ";
+    resultElements[index2+2].innerHTML = relation[i][0]+ ": ";
     resultElements[index2+2].classList.add("code-purple");
 
     resultElements.push(document.createElement("span"));
-    resultElements[index2+3].innerHTML = relation[i][1].trim();
+    resultElements[index2+3].innerHTML = relation[i][1];
     resultElements[index2+3].classList.add("code-yellow");
   }
   index2 = index + 3 * ( relation.length - 1 );
-  console.log(index2);
+  // console.log(index2);
   resultElements.push(document.createElement("span"));
   resultElements[index2+1].classList.add("code-red");
   resultElements[index2+1].innerHTML = "<br>end";
